dc59b330b728be5f5ec1111374d3a54321c8b02a
Caio Santos <caio.sportugal@hotmail.com>
2021-03-17 23:16:29 -0300

    feat: Adiciona o script do index

---
+++ index_script.js
@@ -0,0 +1,9 @@
+function entrar() {
+    var texto = document.getElementById("texto_mensagem")
+    texto.innerText = "É só uma página de exemplo, seus dados não foram salvos, fique tranquilo...."
+}
+
+function cadastrar() {
+    var textocad = document.getElementById("aocadastrar")
+    textocad.innerText = "Seus dados foram enviados para lugar nenhum, obrigado por se cadastrar..."
+}
